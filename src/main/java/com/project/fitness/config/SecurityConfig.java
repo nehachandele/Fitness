38ccc95860c8ffeac638b0ea5f0a37922c66cd45
Neha Chandele <nehachandele2211@gmail.com>
2026-06-19 17:50:02 +0530

    Update application.properties

--- src/main/java/com/project/fitness/config/SecurityConfig.java
+++ src/main/java/com/project/fitness/config/SecurityConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
@@ -23,4 +28,12 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1=User.withUsername("user1")
+        .password("{noop}password1")
+        .build();
+ return new InMemoryUserDetailsManager(user1);
+ }
+
 }
