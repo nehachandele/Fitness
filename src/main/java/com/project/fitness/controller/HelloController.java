d46548596c7e23b8c4c75d5bb0825454858ecfba
Neha Chandele <nehachandele2211@gmail.com>
2026-06-20 16:59:10 +0530

    Security Config  setup

--- src/main/java/com/project/fitness/controller/HelloController.java
+++ src/main/java/com/project/fitness/controller/HelloController.java
@@ -1,14 +1,27 @@
 package com.project.fitness.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
+
 public class HelloController {
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("/hello")
     public String Hello(){
         return "Hello";
+    }
+
+    @GetMapping("/admin/hello")
+    public String sayAdminHello(){
+        return "Heelo, Admin";
+    }
+    @GetMapping("/user/hello")
+    public String sayUserHello(){
+        return "Heelo,User ";
     }
 }
